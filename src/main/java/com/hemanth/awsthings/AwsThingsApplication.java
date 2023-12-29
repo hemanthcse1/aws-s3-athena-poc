fdf0a9a862fcc3a88a27cfa7d681241ac40364f3
hemanthcse1 <hemanthcse1@gmail.com>
2023-12-29 20:52:23 +0530

    Initial commit

---
+++ src/main/java/com/hemanth/awsthings/AwsThingsApplication.java
@@ -0,0 +1,13 @@
+package com.hemanth.awsthings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsThingsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsThingsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hemanth/awsthings/AwsThingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hemanth.awsthings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsThingsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
